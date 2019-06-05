d892015ae0a9060386e91fd4b07dec872a30e680
Patricia Mora <patricia@bazzite.com>
2019-06-05 10:03:28 -0400

    chore(demo): update project structure

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,4 +1,10 @@
-﻿import * as app from 'tns-core-modules/application';
+/*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
+
+import * as app from 'tns-core-modules/application';
 import * as versionTracking from 'nativescript-version-tracking';
 
 app.on('launch', () => {
@@ -6,6 +12,5 @@ app.on('launch', () => {
 
     console.log('versionTracking: ', versionTracking);
 });
-
 
 app.start({ moduleName: 'main-page' });
